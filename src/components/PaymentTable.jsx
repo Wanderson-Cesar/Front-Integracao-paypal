3d409353fac869c95cbcc3624e59b3c1ba4136f5
Wanderson-Cesar <wanderson.xper@gmail.com>
2023-09-16 14:07:10 -0300

    adcionei  o metodo delete e fiz algumas altercoes no codigo

--- src/components/PaymentTable.jsx
+++ src/components/PaymentTable.jsx
@@ -3,53 +3,64 @@ import axios from 'axios';
 import './PaymentTable.css';
 
 function PaymentTable() {
-  const [payments, setPayments] = useState([]);
-  const [editingPayment, setEditingPayment] = useState(null);
-  const [editedStatus, setEditedStatus] = useState('');
+  const [payments, setPayments] = useState([]); // Estado para armazenar os pagamentos
+  const [editingPayment, setEditingPayment] = useState(null); // Estado para rastrear o pagamento em edição
+  const [editedStatus, setEditedStatus] = useState(''); // Estado para rastrear o status editado
 
   useEffect(() => {
+    // Efeito que é executado quando o componente é montado
     const fetchPayments = async () => {
       try {
+        // Obtém a lista de pagamentos do servidor
         const response = await axios.get('http://localhost:5000/api/payment/get-payments');
-        setPayments(response.data);
+        setPayments(response.data); // Define a lista de pagamentos no estado
+
+        setTimeout(async () => {
+          await updatePaymentStatus(response.data);
+          console.log('Status dos pagamentos atualizado para "Pago" após 1 minuto.');
+        }, 30000); // Atualiza o status dos pagamentos para "Pago" após 30 segundos
       } catch (error) {
         console.error('Erro ao obter a lista de pagamentos:', error);
       }
     };
 
-    fetchPayments();
+    fetchPayments(); // Chama a função para buscar os pagamentos ao montar o componente
   }, []);
 
-  const openEditForm = (payment) => {
-    setEditingPayment(payment);
-    setEditedStatus(payment.status);
+  const updatePaymentStatus = async () => {
+    // Função para atualizar o status do pagamento
+    try {
+      if (editingPayment) {
+        // Se há um pagamento em edição
+        await axios.put(`http://localhost:5000/api/payment/update-status/${editingPayment.orderNumber}`, {
+          status: editedStatus,
+        }); // Faz uma solicitação para atualizar o status do pagamento no servidor
+        setEditingPayment(null); // Limpa o estado de edição
+        alert(`Pedido ${editingPayment.orderNumber} editado com sucesso.`);
+      }
+    } catch (error) {
+      console.error('Erro ao atualizar o status para "Pago":', error);
+    }
   };
 
-  const updatePaymentStatus = async () => {
-    if (!editingPayment) return;
-
+  const deletePayment = async (orderNumber) => {
+    // Função para excluir um pagamento
     try {
-      await axios.put(`http://localhost:5000/api/payment/update-status/${editingPayment.orderNumber}`, {
-        status: editedStatus,
-      });
-
-      
-      setPayments((prevPayments) =>
-        prevPayments.map((payment) =>
-          payment.orderNumber === editingPayment.orderNumber
-            ? { ...payment, status: editedStatus }
-            : payment
-        )
-      );
-
-      
-      setEditingPayment(null);
-      setEditedStatus('');
-
-      window.alert(`Status do pedido ${editingPayment.orderNumber} atualizado para "${editedStatus}"`);
+      await axios.delete(`http://localhost:5000/api/payment/delete-payment/${orderNumber}`);
+      // Faz uma solicitação para excluir o pagamento com o número do pedido especificado
+      const updatedPayments = payments.filter((payment) => payment.orderNumber !== orderNumber);
+      setPayments(updatedPayments); // Atualiza a lista de pagamentos após a exclusão
+      alert(`Pedido ${orderNumber} excluído com sucesso.`);
     } catch (error) {
-      console.error('Erro ao atualizar o status de pagamento:', error);
+      console.error('Erro ao excluir o pedido:', error);
     }
+  };
+
+  const openEditForm = (payment) => {
+    // Função para abrir o formulário de edição de pagamento
+    setEditingPayment(payment); // Define o pagamento em edição
+    setEditedStatus(payment.status); // Inicializa o estado de status editado com o status atual
+    alert(`Editando pedido ${payment.orderNumber}`);
   };
 
   return (
@@ -62,9 +73,10 @@ function PaymentTable() {
             <th>Product</th>
             <th>Amount</th>
             <th>Status</th>
-            <th>Name</th> {}
-            <th>CPF</th> {}
-            <th>Editar Status</th> {}
+            <th>Name</th>
+            <th>CPF</th>
+            <th>Edit Status</th>
+            <th>Excluir Pagamento</th> {}
           </tr>
         </thead>
         <tbody>
@@ -84,14 +96,17 @@ function PaymentTable() {
                   payment.status
                 )}
               </td>
-              <td>{payment.name}</td> {}
-              <td>{payment.cpf}</td> {}
+              <td>{payment.name}</td>
+              <td>{payment.cpf}</td>
               <td>
                 {editingPayment && editingPayment.orderNumber === payment.orderNumber ? (
                   <button onClick={updatePaymentStatus}>Salvar</button>
                 ) : (
                   <button onClick={() => openEditForm(payment)}>Editar</button>
                 )}
+              </td>
+              <td>
+                <button onClick={() => deletePayment(payment.orderNumber)}>Excluir</button>
               </td>
             </tr>
           ))}
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,48 +1,54 @@
 import React, { useState, useRef, useEffect } from 'react';
-import gif from '../image/giphy.gif';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import gif from '../image/giphy.gif'; 
+import axios from 'axios'; 
+import { useParams } from 'react-router-dom'; 
 
 function Product({ products }) {
-  const { productId } = useParams();
-  const parsedProductId = parseInt(productId);
-  const product = products.find((p) => p.id === parsedProductId);
+  const { productId } = useParams(); // Obtenção do parâmetro "productId" da URL usando useParams
+  const parsedProductId = parseInt(productId); // Conversão do "productId" para um número inteiro
+  const product = products.find((p) => p.id === parsedProductId); // Busca do produto correspondente com base no ID
 
-  const [paidFor, setPaidFor] = useState(false);
-  const [error, setError] = useState(null);
-  const [formData, setFormData] = useState({ name: '', cpf: '' });
-  const [paymentAllowed, setPaymentAllowed] = useState(false);
-  const paypalRef = useRef();
+  const [paidFor, setPaidFor] = useState(false); // Estado para rastrear se o pagamento foi efetuado com sucesso
+  const [error, setError] = useState(null); // Estado para rastrear erros de pagamento
+  const [formData, setFormData] = useState({ name: '', cpf: '' }); // Estado para armazenar dados do formulário
+  const [paymentAllowed, setPaymentAllowed] = useState(false); // Estado para determinar se o pagamento é permitido
+  const [paymentStatus, setPaymentStatus] = useState(''); // Estado para armazenar o status do pagamento
+  const paypalRef = useRef(); // Referência para o elemento de botões do PayPal
 
   const handleInputChange = (e) => {
+    // Função para lidar com mudanças nos campos de entrada e atualizar os dados do formulário
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const { name, cpf } = formData;
+  const { name, cpf } = formData; // Desestruturação dos dados do formulário
 
   useEffect(() => {
+    // Efeito para validar os campos do formulário e permitir o pagamento
     if (!product) {
-      console.error('Produto não encontrado');
+      console.error('Produto não encontrado'); // Exibir um erro no console se o produto não for encontrado
       return;
     }
 
-    const cleanedCpf = cpf.replace(/[\.-]/g, '');
+    const cleanedCpf = cpf.replace(/[\.-]/g, ''); // Remoção de pontos e traços do CPF
     if (name && cleanedCpf.length === 11 && /^\d+$/.test(cleanedCpf)) {
-      setPaymentAllowed(true);
+      setPaymentAllowed(true); // Permitir o pagamento se o nome e o CPF forem válidos
     } else {
-      setPaymentAllowed(false);
+      setPaymentAllowed(false); // Impedir o pagamento se os campos não forem válidos
     }
   }, [product, name, cpf]);
 
   const generateOrderNumber = () => {
+    // Função para gerar um número de pedido aleatório
     return Math.floor(Math.random() * 10000).toString();
   };
 
   const handlePay = async () => {
-    const orderNumber = generateOrderNumber();
+    // Função para lidar com o pagamento
+    const orderNumber = generateOrderNumber(); // Gerar um número de pedido aleatório
 
     const actions = window.paypal.Buttons({
+      // Definir ações dos botões do PayPal
       createOrder: (data, actions) => {
         return actions.order.create({
           purchase_units: [
@@ -59,41 +65,43 @@ function Product({ products }) {
       },
       onApprove: async (data, actions) => {
         const order = await actions.order.capture();
-        setPaidFor(true);
-        console.log(order);
+        setPaidFor(true); // Definir o estado como pago com sucesso
+        setPaymentStatus('Pago'); // Definir o status do pagamento como "Pago"
 
         const paymentData = {
           product: product.name,
           amount: product.price,
-          status: 'Processando Pagamento',
+          status: 'Pago',
           customerName: name,
           customerCPF: cpf,
           orderNumber,
         };
         console.log('Dados de pagamento a serem enviados para o servidor:', paymentData);
 
-       
-        await axios.post('http://localhost:5000/api/payment/save-payment', paymentData);
+        await axios.post('http://localhost:5000/api/payment/save-payment', paymentData); // Enviar dados de pagamento para o servidor
       },
       onError: (err) => {
-        setError(err);
-        console.error(err);
+        setError(err); // Definir o estado de erro em caso de erro de pagamento
+        setPaymentStatus('Falha no pagamento'); // Definir o status do pagamento como "Falha no pagamento"
+        console.error(err); // Exibir o erro no console
       },
     });
-    actions.render(paypalRef.current);
+    actions.render(paypalRef.current); // Renderizar botões do PayPal usando a referência
   };
 
   if (!product) {
-    return <div>Produto não encontrado</div>;
+    return <div>Produto não encontrado</div>; // Renderizar uma mensagem se o produto não for encontrado
   }
 
   if (paidFor) {
     return (    
       <main className='paypal-form2'>
         <div className='desc2'>
-          <h1>Pagamento pré-aprovado! Seu pagamento pode levar até 30 minutos para ser aprovado no sistema, seu pagamento será processando em breve. {product.name}!</h1>
+        {paidFor && <p>Status do pagamento: {paymentStatus}</p>} {/* Exibir o status do pagamento */}
+          <h1> Pagamento efetuado com sucesso! Seu pagamento pode levar até 30 minutos para ser processado no sistema. {product.name}!</h1>
           <img alt={product.description} src={gif} />
           <a href="/"> Clique aqui para voltar</a>
+         
         </div>
       </main>
     );
@@ -101,7 +109,7 @@ function Product({ products }) {
 
   return (
     <main className='paypal-form'>
-      {error && <div>Uh oh, ocorreu um erro! {error.message}</div>}
+      {error && <div>Uh oh, ocorreu um erro! {error.message}</div>} {/* Exibir erro de pagamento, se houver */}
       <section className='descricaoItem'>
         <div className='desc1'>
           <h1>{product.name}</h1>
@@ -126,7 +134,7 @@ function Product({ products }) {
           {name && cpf && (
             <button onClick={handlePay}>Pagar</button>
           )}
-          <p>Por favor, preencha seu nome e CPF para continuar.</p>
+          {!paymentAllowed && <p>Por favor, preencha seu nome e CPF para continuar.</p>}
           <div ref={paypalRef} />
         </div>
       </section>
